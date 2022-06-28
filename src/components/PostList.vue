fed1d708cf7931e9cbf24333cbadfaaf70bccc11
Ivan Klesov <ivklesov@gmail.com>
2022-06-28 16:34:44 +0300

    feat: add search and pagination

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
-    <post-item
-      :key="post.id"
-      v-for="post in posts"
-      :post="post"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+        :key="post.id"
+        v-for="post in posts"
+        :post="post"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else style="color: red">
     Список пользователей пуст
@@ -29,4 +31,23 @@ export default {
 </script>
 
 <style>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.post-list-move {
+  transition: transform 0.5s ease;
+}
 </style>
---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,45 @@
+<template>
+  <select :value="modelValue" @change="changeOption">
+    <option disabled value="">Выберите фильтр</option>
+    <option
+      v-for="option in options"
+      :key='option.value'
+      :value="option.value"
+    >
+      {{option.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'my-select',
+  props: {
+    modelValue: {
+      type: String
+    },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    changeOption(e) {
+      this.$emit('update:modelValue', e.target.value);
+    }
+  }
+}
+</script>
+
+<style>
+
+.input {
+  width: 100%;
+
+  padding: 10px 15px;
+  margin-top: 15px;
+
+  border: 1px solid teal;
+}
+
+</style>
